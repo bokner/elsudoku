eaa667671994d053a5436dd56dc89745013833af
Boris Okner <boris.okner@gmail.com>
2026-01-10 16:57:04 -0500

    First commit

---
+++ lib/elsudoku.ex
@@ -0,0 +1,18 @@
+defmodule Elsudoku do
+  @moduledoc """
+  Documentation for `Elsudoku`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elsudoku.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elsudoku_test.exs
@@ -0,0 +1,8 @@
+defmodule ElsudokuTest do
+  use ExUnit.Case
+  doctest Elsudoku
+
+  test "greets the world" do
+    assert Elsudoku.hello() == :world
+  end
+end
